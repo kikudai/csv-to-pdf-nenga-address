e340f1e08e718fd8b194d4db05e30c9e8b8dc3ca
Daichi Kikuchi <king.show.kikudai@gmail.com>
2021-12-24 08:05:35 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,55 @@
+# 年賀状宛名 csv の pdf 化
+csvファイル（はがきデザインキット）から、年賀状宛名をpdfにして印刷できるようにする。
+
+## 概要
+はがきデザインキットが機能停止してしまい、過去のcsvなど利用できなくなっていた。
+これはもう python で印刷するしかない。
+
+## 機能
+はがきデザインキットから書き出した宛名csvをpdfにする
+
+## サンプルコマンド
+```
+# 年賀状レイアウト付きでpdf出力（pdfでレイアウト確認をしたいとき）
+python nenga_csv_to_pdf.py --layout
+
+# 年賀状レイアウトなしでpdf出力（はがき宛名面印刷をするとき）
+python nenga_csv_to_pdf.py
+```
+## サンプル
+![sample_nenga](/sample_nanga.png)
+
+## 事前準備
+### python のインストール
+* [Python環境構築ガイド - python.jp|https://www.python.jp/install/install.html] などを参考に python をインストール
+
+### コマンド実行環境構築
+```
+git clone https://github.com/kikudai/csv-to-pdf-nenga-address.git
+cd csv-to-pdf-nenga-address.git
+
+# ライブラリインストール
+pip install -r require
+```
+
+### コマンド実行
+owner_info.csv、address_list を nenga_csv_to_pdf.py と同じディレクトリ配下においてコマンド実行する。
+address_list.csv は、はがきデザインキットから書き出したcsvファイルのことです。
+owner_info.csv はサンプルcsvを参考に自身の住所・氏名の csv を作成してください。
+```
+# まずはサンプルcsvファイルをコピー
+cp ./sample_csv/*.csv ./
+
+# コマンド実行後、address.pdf が作成される
+python nenga_csv_to_pdf.py --layout
+```
+
+## 注意事項
+* csvファイル（年賀状住所、送り元情報）の文字コードは utf8 にする
+* pdf を Adobe acrobat reader DC などで開きっぱなしのとき、コマンド実行するとエラー（PermissionError: [Errno 13] Permission denied: 'address.pdf'）になるので閉じて実行する
+
+## 感想
+* 作成期間約2日、本当は半日くらいで作りたかった
+* ここのコードをもとに作成しました。ありがたや。これがなければ、もう1日くらいかかっていそう  
+https://github.com/satemochi/saaaaah/blob/master/misc/new_year_card/write_address.py 
+* 恥ずかしコード、リファクタリングすれば半分くらいのコードになりそう
---
+++ nenga_csv_to_pdf.py
@@ -0,0 +1,360 @@
+import argparse
+from typing import List, Dict
+import pandas as pd
+import numpy as np
+from PIL import Image
+from pandas.core.frame import DataFrame
+from reportlab.pdfgen import canvas
+from reportlab.pdfbase import pdfmetrics
+from reportlab.pdfbase.pdfmetrics import getFont
+from reportlab.pdfbase.cidfonts import UnicodeCIDFont
+from reportlab.lib.units import cm
+
+
+def set_up(fname='address.pdf') -> canvas.Canvas:
+    pdfmetrics.registerFont(UnicodeCIDFont('HeiseiMin-W3', isVertical=True))
+    pdfmetrics.registerFont(UnicodeCIDFont('HeiseiKakuGo-W5'))
+    pdf = canvas.Canvas(fname)
+    pdf.setPageSize((10 * cm, 14.8 * cm))
+    return pdf
+
+
+def draw_image(pdf: canvas.Canvas):
+    """
+    画像
+    """
+    image = Image.open('./layout_nenga.png')
+    pdf.drawInlineImage(image, 2, 0, width=10 * cm, height=14.8 * cm)
+
+
+def draw_send_name(pdf: canvas.Canvas, df_send_name: DataFrame):
+    """
+    宛先の氏名（連名にも対応）
+    """
+    # 文字サイズ
+    SEND_ADDRES_1_FONT_SIZE = 20
+
+    pdf.setFont('HeiseiMin-W3', SEND_ADDRES_1_FONT_SIZE)
+
+    base_y = 10.5 * cm
+
+    if len(df_send_name) == 1:
+        base_x = 5.1 * cm
+    elif len(df_send_name) == 2:
+        base_x = 6.1 * cm
+    elif len(df_send_name) == 3:
+        base_x = 6.1 * cm
+    elif len(df_send_name) == 4:
+        base_x = 6.6 * cm
+    elif len(df_send_name) == 5:
+        base_x = 7.1 * cm
+
+    MARGIN = 1 * cm
+
+    # 連名の横間隔
+    side_width = 0.8 * cm
+
+    names = edit_names(df_send_name)
+
+    for i_owner_names, name in enumerate(names):
+        x = base_x - side_width * i_owner_names
+        y = base_y
+        for i, c in enumerate(name):
+            y = base_y - MARGIN * i
+            pdf.drawString(x, y, c)
+
+
+def edit_names(df_name: DataFrame):
+
+    df_name = df_name.fillna('')
+
+    max_sei = df_name[0].map(lambda x: len(x)).max()
+    max_mei = df_name[1].map(lambda x: len(x)).max()
+
+    owner_names = []
+    for sei_mei in df_name.values:
+        # 姓・名ループ
+        owner_name = ""
+        for i_sei_mei, sm in enumerate(sei_mei):
+            # 姓が空の場合は全角スペースで埋める
+            if i_sei_mei == 0 and len(sm) == 0:
+                owner_name = "　" * max_sei
+            else:
+                # sprint(f'{sm} {max_mei}')
+                if i_sei_mei == 1 and len(sm) < max_mei:
+                    owner_name = owner_name + sm + "　" * (max_mei - len(sm))
+                else:
+                    owner_name = owner_name + sm
+        owner_names.append(owner_name)
+
+    return owner_names
+
+
+def draw_name_owner(pdf: canvas.Canvas, names: DataFrame):
+    """
+    送り元氏名（連名にも対応）
+    """
+    # 文字サイズ
+    SEND_ADDRES_1_FONT_SIZE = 9
+
+    pdf.setFont('HeiseiMin-W3', SEND_ADDRES_1_FONT_SIZE)
+
+    base_y = 5.8 * cm
+
+    if len(names) == 1:
+        base_x = 0.5 * cm
+    elif len(names) == 2:
+        base_x = 1.1 * cm
+    elif len(names) == 3:
+        base_x = 1.5 * cm
+
+    MARGIN = 0.6 * cm
+
+    # 連名の横間隔
+    side_width = 0.4 * cm
+
+    # 連名ループ
+    owner_names = edit_names(names)
+
+    for i_owner_names, name in enumerate(owner_names):
+        x = base_x - side_width * i_owner_names
+        y = base_y
+        for i, c in enumerate(name):
+            y = base_y - MARGIN * i
+            pdf.drawString(x, y, c)
+
+
+def draw_zipcode(pdf: canvas.Canvas, zipcode):
+    """
+    郵便番号
+    """
+    # 文字サイズ
+    ZIP_CODE_FONT_SIZE = 15
+    # 文字間隔（パディング）
+    ZIP_CODE_FONT_PADDING = 24
+    # 文字間隔（マージン）
+    ZIP_CODE_FONT_MARGIN = 0.15 * cm
+
+    if "-" in zipcode:
+        zipcode = zipcode.replace('-', '')
+
+    pdf.setFont('HeiseiKakuGo-W5', ZIP_CODE_FONT_SIZE)
+    width_char = getFont('HeiseiKakuGo-W5').stringWidth(
+        zipcode[0], ZIP_CODE_FONT_PADDING) + ZIP_CODE_FONT_MARGIN
+    for i, c in enumerate(zipcode):
+        x = 4.53 * cm + width_char * i
+        y = 13 * cm
+        pdf.drawString(x, y, c)
+
+
+def draw_zipcode_owner(pdf: canvas.Canvas, zipcode):
+    """
+    送り元郵便番号
+    """
+    # 文字サイズ
+    _ZIP_CODE_FONT_SIZE = 13
+    # 文字間隔（パディング）
+    _ZIP_CODE_FONT_PADDING = 10
+    # 文字間隔（マージン）
+    _ZIP_CODE_FONT_MARGIN = 0.15 * cm
+
+    if "-" in zipcode:
+        zipcode = zipcode.replace('-', '')
+
+    pdf.setFont('HeiseiKakuGo-W5', _ZIP_CODE_FONT_SIZE)
+    _width_char = getFont('HeiseiKakuGo-W5').stringWidth(
+        zipcode[0], _ZIP_CODE_FONT_PADDING) + _ZIP_CODE_FONT_MARGIN
+
+    for i, c in enumerate(zipcode):
+        # 微調整
+        if i > 2:
+            x = 0.81 * cm + _width_char * i
+        else:
+            x = 0.66 * cm + _width_char * i
+        y = 2 * cm
+        pdf.drawString(x, y, c)
+
+
+def edit_address(address: List):
+    """
+    宛先住所に適度に改行をいれて整形
+    （TODO: リファクタリングしたい）
+    """
+    list_edited = []
+    address_length = 0
+    address_char = ""
+
+    for ad in address:
+        ad = str(ad)
+        address_length = address_length + len(ad)
+        if (address_length > 16):
+            list_edited.append(address_char)
+            # リセット
+            address_length = len(ad)
+            address_char = ad
+            continue
+
+        address_char = address_char + ad
+
+    list_edited.append(address_char)
+    return list_edited
+
+
+def draw_send_address(pdf: canvas.Canvas, address: List):
+    """
+    宛先住所
+    """
+    # 文字サイズ
+    SEND_ADDRES_1_FONT_SIZE = 14
+
+    pdf.setFont('HeiseiMin-W3', SEND_ADDRES_1_FONT_SIZE)
+
+    for addr_i, addr_word in enumerate(address):
+        addr_x = 8.9 * cm - 0.6 * cm * addr_i
+
+        for i, c in enumerate(addr_word):
+            y = 12.3 * cm - (0.5 * cm * i)
+
+            # 住所の番地前後のハイフンの文字サイズ変更
+            if c in '-ー－':
+                pdf.setFont('HeiseiMin-W3', 8)
+            else:
+                pdf.setFont('HeiseiMin-W3', SEND_ADDRES_1_FONT_SIZE)
+
+            pdf.drawString(addr_x, y, chinese_numeral(c))
+
+
+def draw_send_address_owner(pdf: canvas.Canvas, address: List):
+    """
+    送り元住所
+    """
+    # 文字サイズ
+    SEND_ADDRES_1_FONT_SIZE = 10
+
+    pdf.setFont('HeiseiMin-W3', SEND_ADDRES_1_FONT_SIZE)
+
+    for addr_i, addr_word in enumerate(address):
+        addr_x = 2.8 * cm - 0.5 * cm * addr_i
+
+        for i, c in enumerate(addr_word):
+            y = 8.5 * cm - (11 * i)
+
+            # 住所の番地前後のハイフンの文字サイズ変更
+            if c in '-ー－':
+                pdf.setFont('HeiseiMin-W3', 6)
+            else:
+                pdf.setFont('HeiseiMin-W3', SEND_ADDRES_1_FONT_SIZE)
+
+            pdf.drawString(addr_x, y, chinese_numeral(c))
+
+
+def chinese_numeral(s):
+    convert_table = {ord(u'0'): u'〇', ord(u'1'): u'一', ord(u'2'): u'二',
+                     ord(u'3'): u'三', ord(u'4'): u'四', ord(u'5'): u'五',
+                     ord(u'6'): u'六', ord(u'7'): u'七', ord(u'8'): u'八',
+                     ord(u'9'): u'九', ord(u'-'): u'|', ord(u'－'): u'|', 
+                     ord(u'ー'): u'|'}
+    return s.translate(convert_table)
+
+
+def drow_owner_info(pdf, owner_info: Dict):
+    """
+    送り元情報
+    """
+    zipcode = owner_info["zipcode"]
+    draw_zipcode_owner(pdf, zipcode)
+    address = owner_info["address"]
+    draw_send_address_owner(pdf, address)
+    names = owner_info["names"]
+    draw_name_owner(pdf, names)
+
+
+if __name__ == '__main__':
+
+    # コマンドライン引数
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--layout", help="optional", action="store_true")
+    args = parser.parse_args()
+
+    pdf = set_up()
+
+    df_send = pd.read_csv('address_list.csv').fillna('')
+    df_owner = pd.read_csv('owner_info.csv').fillna('')
+
+    # 送り元郵便番号
+    owner_zipcode = df_owner.iloc[0]['郵便番号(自宅欄)']
+    # 送り元住所
+    owner_address = [
+        df_owner.iloc[0]['自宅住所(都道府県)'],
+        df_owner.iloc[0]['自宅住所(市区町村)'],
+        df_owner.iloc[0]['自宅住所(番地等)'],
+        df_owner.iloc[0]['自宅住所(建物名)'],
+    ]
+    # 送り元氏名
+    owner_names = [
+        [str(df_owner.iloc[0]["氏名(姓)"]), str(df_owner.iloc[0]["氏名(名)"])],
+        [str(df_owner.iloc[0]["連名1(姓:自宅欄)"]),
+         str(df_owner.iloc[0]["連名1(名:自宅欄)"])],
+        [str(df_owner.iloc[0]["連名2(姓:自宅欄)"]),
+         str(df_owner.iloc[0]["連名2(名:自宅欄)"])],
+    ]
+    df_names_owner = pd.DataFrame(
+        data=owner_names
+    )
+
+    for i, row in df_send.iterrows():
+        # 年賀状レイアウト出力
+        if args.layout:
+            draw_image(pdf)
+
+        # 送り先住所
+        send_address = edit_address(
+            [
+                str(row['自宅住所(都道府県)']),
+                str(row['自宅住所(市区町村)']),
+                str(row['自宅住所(番地等)']),
+                str(row['自宅住所(建物名)']),
+            ]
+        )
+
+        # 送り先氏名
+        send_name = [
+            [str(row["氏名(姓)"]), str(row["氏名(名)"]), str(row["敬称"])],
+            [str(row["連名1(姓:自宅欄)"]),
+             str(row["連名1(名:自宅欄)"]),
+                str(row["連名1(敬称:自宅欄)"])],
+            [str(row["連名2(姓:自宅欄)"]),
+             str(row["連名2(名:自宅欄)"]),
+                str(row["連名2(敬称:自宅欄)"])],
+            [str(row["連名3(姓:自宅欄)"]),
+             str(row["連名3(名:自宅欄)"]),
+                str(row["連名3(敬称:自宅欄)"])],
+            [str(row["連名4(姓:会社欄)"]),
+             str(row["連名4(名:会社欄)"]),
+                str(row["連名4(敬称:会社欄)"])],
+            [str(row["連名5(姓:会社欄)"]),
+             str(row["連名5(名:会社欄)"]),
+                str(row["連名5(敬称:会社欄)"])],
+        ]
+        df_send_name: DataFrame = pd.DataFrame(data=send_name)
+        df_send_name = df_send_name.replace("", np.nan)
+        df_send_name = df_send_name.dropna(how='all', axis=0)
+
+        draw_zipcode(pdf, str(row['郵便番号(自宅欄)']))
+        draw_send_address(pdf, send_address)
+        draw_send_name(pdf, df_send_name)
+
+        # 送り元
+        owner_info = {
+            "zipcode": owner_zipcode,
+            "address": edit_address(owner_address),
+            "names": df_names_owner
+        }
+
+        drow_owner_info(pdf, owner_info)
+
+        # 改ページ
+        pdf.showPage()
+
+    # PDF保存
+    pdf.save()
